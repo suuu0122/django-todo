306d0f0a7f1477b36dd271c80fede089c1b5376f
suuu0122 <suuu0122lab@gmail.com>
2023-07-16 14:26:55 +0900

    add __str__ method

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo  = models.TextField()
+    
+
+    def __str__(self):
+        return self.title
