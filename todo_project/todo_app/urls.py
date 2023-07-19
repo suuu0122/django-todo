4bc44460c4b19b0445119124354f0fdacc06bfd3
suuu0122 <suuu0122lab@gmail.com>
2023-07-19 23:03:27 +0900

    add delete view

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list/', views.TodoList.as_view(), name="list"),
     path('detail/<int:pk>', views.TodoDetail.as_view(), name="detail"),
     path('create/', views.TodoCreate.as_view(), name="create"),
+    path('delete/<int:pk>', views.TodoDelete.as_view(), name="delete"),
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
 
 from .models import Todo
 
@@ -21,4 +21,11 @@ class TodoCreate(CreateView):
     template_name = "todo_app/create.html"
     model         = Todo
     fields        = ("title", "memo", "priority", "duedate")
+    success_url   = reverse_lazy("todo_app:list")
+
+
+
+class TodoDelete(DeleteView):
+    template_name = "todo_app/delete.html"
+    model         = Todo
     success_url   = reverse_lazy("todo_app:list")
