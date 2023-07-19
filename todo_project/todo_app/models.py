6cf5b6d0496b17a9af851ea3fa5abd01c0c03f47
suuu0122 <suuu0122lab@gmail.com>
2023-07-19 20:24:54 +0900

    added new field priority and dudate

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 
+CHOICE = (("danger", "high"), ("warning", "normal"), ("primary", "low"))
+
+
+
 class Todo(models.Model):
-    title = models.CharField(max_length=100)
-    memo  = models.TextField()
-    
+    title    = models.CharField(max_length=100)
+    memo     = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE
+    )
+    duedate  = models.DateField()
+
 
     def __str__(self):
         return self.title
